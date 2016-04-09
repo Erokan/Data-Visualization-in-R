db4ec84ec51f1c2d054bb6fa869d795ace6202a5
erokan <erokan@gmail.com>
2016-04-10 02:18:38 +0300

    Import data into R

---
+++ 1-Introduction/008-Import data into R.R
@@ -0,0 +1,8 @@
+# Import data
+# setdw ile dosya yolu kolay kullanım için kaydedilebilir.
+
+setwd("~/Google Drive/repos/r/Data Visualization/1-Introduction/")
+myData <- read.csv("008-data.csv")
+myData
+View(myData) # Ayrı bir pencerede datayı gösterir.
+
