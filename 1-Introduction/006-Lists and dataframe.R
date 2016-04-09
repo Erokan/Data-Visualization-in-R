7286ec7ad78049534679fe363102dba3a85c3bbf
erokan <erokan@gmail.com>
2016-04-10 01:45:15 +0300

    List and dataFrame

--- 1-Introduction/006-Lists and dataframe.R
+++ 1-Introduction/006-Lists and dataframe.R
@@ -5,3 +5,12 @@ s<-c("aa","bb","cc","dd","ee")
 b=c(TRUE,FALSE,T,F)
 x=list(n,s,b,3)   # x contains copy of n,s,b
 x
+
+
+# Working with dataFrame
+
+n1<-c(2,3,5)
+s1<-c("aa","bb","cc")
+b1=c(TRUE,FALSE,T)
+df=data.frame(n1,s1,b1) #df is a data frame
+df
