acceaa0dd0934fd341cacede24c89db91f69fd47
erokan <erokan@gmail.com>
2016-04-10 02:46:17 +0300

    xlsx data

--- 1-Introduction/008-Import data into R.R
+++ 1-Introduction/008-Import data into R.R
@@ -6,3 +6,31 @@ myData <- read.csv("008-data.csv")
 myData
 View(myData) # Ayrı bir pencerede datayı gösterir.
 
+# writing csv file
+write.csv(myData,file = "008-newData.csv")
+
+
+
+# reading from Excel (xlsx) - çalışmadı!
+install.packages("xlsx")                  # yeni xlsx dosyaları okuyamıyor.
+myData<-read.xlsx("008-data.xlsx")
+file.exists("008-data.xlsx") # true döndürüse dosya var demektir.
+
+# reading from Excel (xlsx) - çalışmadı!
+install.packages("XLConnect")
+theData <- XLConnect::readWorksheet(loadWorkbook("008-data.xlsx"),sheet=1)
+
+# reading from Excel (xlsx) - çalıştı :)
+install.packages("readxl")
+library(readxl)
+read_excel("008-data.xlsx")
+# read_excel("my-spreadsheet.xls", sheet = 2)
+# read_excel("my-spreadsheet.xls", sheet = "data")
+
+# R içindeki datasetleri gösterir
+data()
+?women
+View(women)
+install.packages("ggplot2")
+require(ggplot2)
+View(diamonds)
